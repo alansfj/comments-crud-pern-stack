d2477b5034d7cada2bb9704555506c48296059f5
alansfj <alansfj348@gmail.com>
2022-08-19 11:48:57 -0500

    productionConfig obj modified

--- db.js
+++ db.js
@@ -10,7 +10,10 @@ const devConfig = {
 }
 
 const productionConfig = {
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 }
 
 const pool = new Pool(process.env.NODE_ENV === 'production' ? productionConfig : devConfig);
